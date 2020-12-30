865371cc4db7f05d7c126beaa9a31005eb81cbce
Al Mamun Khan <almamunkhan04@gmail.com>
2020-12-30 22:02:18 +0600

    Update App.js

--- ui/src/App.js
+++ ui/src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   // constructor(props) {
     state = {
       response: [],
-      endpoint: "ws://0.0.0.0:8083/ws",
+      endpoint: "ws://0.0.0.0:8084/ws",
       loading: false,
     }
   // }
@@ -51,19 +51,7 @@ class App extends Component {
                   alt=""
                 />
               </div>
-              <div style={{ fontSize: 25, textAlign: "center", margin:"20px 70px" }}>
-                <h3>Detected Face</h3>
-                <br />
-                <img
-                  src={
-                    response.detection && `data:image/gif;base64,${response.detection}`
-                  }
-                  alt=""
-                  width="300"
-                /> <br />
-                  {response.name ? <>Name: {response.name} <br /></> : null}
-                  {response.score ? <>Score: {response.score} <br /></> : null}
-              </div>
+             
             </>
           ) : null}
         </div>
